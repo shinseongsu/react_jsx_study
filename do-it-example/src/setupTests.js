1af359d951b9f745e3def73aafd6209748ef9986
Shin seongsu <tlstjdtn321@naver.com>
2020-08-13 23:32:11 +0900

    test 코드 추가(Text)

--- do-it-example/src/setupTests.js
+++ do-it-example/src/setupTests.js
@@ -1,5 +1,10 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16.3';
+import * as Aphrodite from 'aphrodite';
+import * as AphroditeNoImportant from 'aphrodite/no-important';
+
+Aphrodite.StyleSheetTestUtils.suppressStyleInjection();
+AphroditeNoImportant.StyleSheetTestUtils.suppressStyleInjection();
 
 configure({ adapter: new Adapter() });
 
