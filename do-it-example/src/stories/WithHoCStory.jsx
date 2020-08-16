89da74af8dab81b30cd33add385e8b4760ee2c4b
Shin seongsu <tlstjdtn321@naver.com>
2020-08-16 23:35:37 +0900

    하이어오더 컴포넌트

--- do-it-example/src/stories/WithHoCStory.jsx
+++ do-it-example/src/stories/WithHoCStory.jsx
@@ -3,21 +3,22 @@ import { storiesOf } from '@storybook/react';
 
 import Button from '../04/Button';
 import Text from '../04/Text';
+import withLoading from '../05/withLoading';
 import withHoc from '../05/withHoC';
 
-const ButtonWithHoc = withHoc(Button);
-const TextWithHoc = withHoc(Text);
+const ButtonWithHoc = withLoading(<Button disabled>로딩 중...</Button>)(Button);
+const TextWithHoc = withLoading('로딩 중')(Text);
 
-storiesOf('WithHoc', module)
-  .addWithJSX('기본 설정', () => {
+storiesOf('withLoading', module)
+  .addWithJSX('기본 설정', () => (
     <div>
       <ButtonWithHoc>안녕하세요</ButtonWithHoc>
       <TextWithHoc>안녕하세요</TextWithHoc>
-    </div>;
-  })
-  .addWithJSX('large 예제', () => (
+    </div>
+  ))
+  .addWithJSX('isLoading 예제', () => (
     <div>
-      <ButtonWithHoc large>안녕하세요</ButtonWithHoc>
-      <TextWithHoc large>안녕하세요</TextWithHoc>
+      <ButtonWithHoc isLoading>안녕하세요</ButtonWithHoc>
+      <TextWithHoc isLoading>안녕하세요</TextWithHoc>
     </div>
   ));
