2942a0011ea7cbbf6855e2ae194e06ef1c6e104d
Shin seongsu <tlstjdtn321@naver.com>
2020-08-24 23:22:19 +0900

    modal, 회원가입 폼

--- do-it-example/src/stories/ModalStory.jsx
+++ do-it-example/src/stories/ModalStory.jsx
@@ -5,6 +5,13 @@ import Modal from '../06/Modal';
 import Button from '../04/Button';
 import Text from '../04/Text';
 import ButtonWithModal from '../06/ButtonWithModal';
+import ModalProvider, { Consumer } from '../06/ModalProvider';
+
+import ModalProviderWithKey, {
+  CONFIRM_DELETE_MODAL,
+  CREATE_MEMBER_MODAL,
+} from '../06/ModalProviderWithKey';
+import { Consumer as ModalConsumer } from '../06/ModalContext';
 
 storiesOf('Modal', module)
   .addWithJSX('기본 설정', () => (
@@ -16,4 +23,31 @@ storiesOf('Modal', module)
       <Button>닫기</Button>
     </Modal>
   ))
-  .addWithJSX('ButtonWithModal', () => <ButtonWithModal />);
+  .addWithJSX('ButtonWithModal', () => <ButtonWithModal />)
+  .addWithJSX('ModalProvider', () => (
+    <ModalProvider>
+      <div>
+        <Text>다음 버튼 눌러 모달을 실행합니다.</Text>
+        <Consumer>{({ openModal }) => <Button onPress={() => openModal()}>삭제</Button>}</Consumer>
+      </div>
+    </ModalProvider>
+  ))
+  .addWithJSX('ModalProviderWithKey', () => (
+    <ModalProviderWithKey>
+      <div>
+        <Text>다음 버튼 눌러 모달을 실행합니다.</Text>
+        <ModalConsumer>
+          {({ openModal }) => (
+            <Button onPress={() => openModal(CONFIRM_DELETE_MODAL, { id: 1, name: '상품1' })}>
+              모달 열기
+            </Button>
+          )}
+        </ModalConsumer>
+        <ModalConsumer>
+          {({ openModal }) => (
+            <Button onPress={() => openModal(CREATE_MEMBER_MODAL)}>회원 가입</Button>
+          )}
+        </ModalConsumer>
+      </div>
+    </ModalProviderWithKey>
+  ));
